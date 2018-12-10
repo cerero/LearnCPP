246dc088bea568d9929f0d8b111b21fde9dce41a
王永杰 <wangyongjie@kugou.net>
2018-12-10 18:32:40 +0800

    day05#cr:通过实际的内存布局，来计算出id的地址偏移量

--- day05/dm01_结构体偏移量.c
+++ day05/dm01_结构体偏移量.c
@@ -8,13 +8,20 @@ typedef struct Teacher {
   int id
 } Teacher;
 
+//可以通过age的内存地址，去求整个Teacher的内存地址
 int main(int argc, char **args, char **env) {
   Teacher *p = NULL;
-  int offsetOfId = &(p->id);
   //不能访问id，因为p是NULL指针
   // printf("id = %d, offsetOfId %d\n", p->id, offsetOfId);
-
+  int offsetOfId = &(p->id);
+  offsetOfId = &(((Teacher *) 0)->id);
   //但通过NULL指针，可以用来来计算结构体字段的地址偏移量 ， 牛逼
+  printf("offsetOfId %d\n", offsetOfId);
+
+  Teacher teacher;
+  p = &teacher;
+  //通过实际的内存布局，来计算出id的地址偏移量
+  offsetOfId = (int)(&(p->id)) - (int)p;
   printf("offsetOfId %d\n", offsetOfId);
   return 0;
 }
